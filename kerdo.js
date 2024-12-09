8d7339a46680517ff3f93faec68560871f5931e9
Puppak <123452210+Puppak@users.noreply.github.com>
2024-12-09 20:51:37 +0100

    létre hoztam a jst csináltam még mellé ezt azt
    plusz minusz gomb még finomítást igényel, létre hoztam  a főfelület oldalat

---
+++ kerdo.js
@@ -0,0 +1,18 @@
+let visszaszamlal = setInterval(szamlalo(),  valos_Ido * 60000)
+let valos_Ido = document.getElementById() 
+
+
+
+function ido_hozzaAdas()
+{
+
+}
+function ido_elvonas()
+{
+
+}
+
+function szamlalo()
+{
+
+}
