e26f12f0a1ddcfb41d195e271aa7b8c415ce4ebf
bonorb <bogdan.norbert@liveedu.mathiasz.hu>
2024-12-11 10:32:42 +0100

    ezeket csinaltam ezen az oran a golokat mar lehet nyomkodni es a normalisan elhelyezni es megcsianltam h minuszba ne tudjon menni

--- kerdo.js
+++ kerdo.js
@@ -1,7 +1,3 @@
-let visszaszamlal = setInterval(szamlalo(),  valos_Ido * 60000)
-let valos_Ido = document.getElementById() 
-
-
 
 function ido_hozzaAdas()
 {
@@ -14,5 +10,32 @@ function ido_elvonas()
 
 function szamlalo()
 {
+
+}
+
+let gol1 = 0;
+let gol2 = 0;
+function csapat1minus(){
+    if (gol1 > 0)
+        gol1--;
+   
+    document.getElementById('csap1').innerHTML = gol1;
+}
+function csapat1plus(){
+     gol1++;
+    
+    document.getElementById('csap1').innerHTML = gol1;
+
+}
+function csapat2minus(){
+    if (gol2 > 0)
+        gol2--;
+   
+    document.getElementById('csap2').innerHTML = gol2;
+}
+function csapat2plus(){
+     gol2++;
+    
+    document.getElementById('csap2').innerHTML = gol2;
 
 }
